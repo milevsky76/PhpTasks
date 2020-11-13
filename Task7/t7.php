57b152de952d4fe37ae07c0002eb5bcd274d2b4d
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:29:54 +0300

    Adding a solution for task 7

---
+++ Task7/t7.php
@@ -0,0 +1,19 @@
+﻿<?php
+    
+    require_once "palindrome.php";
+    require_once "transliteration.php";
+    
+    $phrasalArray = ["Тут как тут", "Коту тащат уток", "15.01.2002 10:51", "Я разуму уму заря", "Искать такси", "Дивен мне вид"];
+    $newPhrasalArray = [];
+    
+    foreach($phrasalArray as $key => $value){
+        $value = $newPhrasalArray[$key] = DoTransliteration($value);
+        
+        if(IsPalindrome($value)){
+            echo "$value - является палиндромом<br>";
+        } else{
+            echo "$value - не является палиндромом<br>";
+        }
+    }
+    
+?>
