ec05ccd37d96b22394e70c9d040f1ee96d5db125
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:35:18 +0300

    Adding a solution for task 8

---
+++ Task8/t8.php
@@ -0,0 +1,20 @@
+﻿<?php
+    
+    function pr($arr){
+        echo "<pre>";
+        print_r($arr);
+        echo "</pre>";
+    }
+
+    $arr = [];
+    $degree = 0;
+    
+    for($i = 0; $i < 8; $i++){
+        for($j = 0; $j < 8; $j++){
+            $arr[$i][$j] = pow(2, $degree++);
+        }
+    }
+    
+    pr($arr);    
+    
+?>
