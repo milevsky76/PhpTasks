da08adfb33a586bb8e4ba8b65db1d70eeed9d157
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:38:47 +0300

    Adding a processing file

---
+++ Task9/send.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+    require_once "CView.php";
+
+    $view = new CView();
+    
+    switch($_REQUEST["type"]){
+        //Ajax календаря
+        case "uny":{
+            $view->ViewUntilNewYear();
+            break;
+        }
+        //Ajax городов
+        case "add":{
+            if($_REQUEST["area"]){
+                $view->ViewSelectCities();
+            }       
+            break;
+        }
+        //Ajax данных
+        default:{
+            $view->ViewValidationData();
+        }
+    }
+?>
