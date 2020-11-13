99aeb16ee410dcd49a617f1abe77e6feae7163c6
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:17:25 +0300

    Adding a solution for task 1

---
+++ Task1/t1.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+    $quantityOdd = 0;
+    
+    for($number = 100; $number >= 50; $number--){
+        if(IsOdd($number)){
+            echo "$number <br>";
+            $quantityOdd++;
+        }
+    }
+    
+    echo "Всего $quantityOdd нечётных чисел";
+    
+    function IsOdd($number){
+        if ($number % 2 != 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+?>
