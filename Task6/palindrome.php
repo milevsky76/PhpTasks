fa96bdd8c0d7b8d1ef915f69771d817c1126ed4a
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:29:47 +0300

    Adding a solution for task 6

---
+++ Task6/palindrome.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+    function IsPalindrome($str){
+        //Удаляем не нужные символы
+        $strFormat = str_replace([" ", ",", ".", ":", "!", "?"], "", $str);
+        
+        //Приводим строку к нижнему регистру
+        $strLower = mb_strtolower($strFormat);
+        
+        //Записываем строку в обратном порядке
+        $strReverse = mb_strrev($strLower);
+        
+        if($strReverse == $strLower){
+            return true;
+        } else{
+            return false;
+        }
+    }
+    
+    function mb_strrev($str){
+        $res = "";
+        
+        for ($i = mb_strlen($str); $i >= 0; $i--) {
+            $res .= mb_substr($str, $i, 1);
+            
+        }
+        
+        return $res;
+    }
+
+?>
---
+++ Task6/t6.php
@@ -0,0 +1,15 @@
+﻿<?php
+    
+    require_once "palindrome.php";
+    
+    $phrasalArray = ["Тут как тут", "Коту тащат уток", "15.01.2002 10:51", "Я разуму уму заря", "Искать такси", "Дивен мне вид"];
+    
+    foreach($phrasalArray as $value) { 
+        if(IsPalindrome($value)){
+            echo "$value - является палиндромом<br>";
+        } else{
+            echo "$value - не является палиндромом<br>";
+        }
+    }   
+
+?>
