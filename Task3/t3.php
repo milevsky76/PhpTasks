727097b98b520937e106e96351e1c7254b909715
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-13 14:25:10 +0300

    Adding a solution for task 3

---
+++ Task3/t3.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+    include "calendar.php";
+    
+    $daysInMonth = 31;
+    $firstDayMonth = "понедельник";
+        
+    $month = GeneratingMonth($daysInMonth, $firstDayMonth);
+    
+    echo "<pre>";
+    print_r($month);
+    echo "</pre>";
+
+?>
